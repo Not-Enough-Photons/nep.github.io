abedf9d2f0116ffbdfd0746eb7a96f535208a368
adamdev <hmullinsk@hotmail.com>
2023-07-25 01:20:47 -0400

    changed video formats

--- project_listing_script.js
+++ project_listing_script.js
@@ -4,19 +4,19 @@ function onHover(event) {
     const background = document.getElementById("video-background");
 
     if(event.target.textContent == "Hitmarkers") {
-        background.src = "img/project_hitmarkers.mp4";
+        background.src = "img/project_hitmarkers.webm";
     }
     else if(event.target.textContent == "Scoreworks") {
-        background.src = "img/project_scoreworks.mp4";
+        background.src = "img/project_scoreworks.webm";
     }
     else if(event.target.textContent == "Paranoia") {
-        background.src = "img/project_paranoia.mp4";
+        background.src = "img/project_paranoia.webm";
     }
     else if(event.target.textContent == "MonoDirector") {
-        background.src = "img/project_monodirector.mp4";
+        background.src = "img/project_monodirector.webm";
     }
     else if(event.target.textContent == "Thrusters") {
-        background.src = "img/project_thrusters.mp4";
+        background.src = "img/project_thrusters.webm";
     }
     
     background.load();

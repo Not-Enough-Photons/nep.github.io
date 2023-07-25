46a6171aa2197b7df0809a0bd7ac7a48d0deaae0
adamdev <hmullinsk@hotmail.com>
2023-07-25 01:57:06 -0400

    games section

--- project_listing_script.js
+++ project_listing_script.js
@@ -1,25 +1,31 @@
 const elements = document.querySelectorAll('#project-parent p#project-button');
+const projectVideos = {
+    "Hitmarkers": "img/project_hitmarkers.webm",
+    "Scoreworks": "img/project_scoreworks.webm",
+    "Paranoia": "img/project_paranoia.webm",
+    "MonoDirector": "img/project_monodirector.webm",
+    "Thrusters": "img/project_thrusters.webm",
+    "Smiler": "img/game_smiler.webm",
+    "Light Night": "img/game_lightnight.webm",
+    "Hide And Seek": "img/game_hideandseek.webm"
+};
+
+var currentVideo = "";
+var activeVideo = "";
 
 function onHover(event) {
     const background = document.getElementById("video-background");
-
-    if(event.target.textContent == "Hitmarkers") {
-        background.src = "img/project_hitmarkers.webm";
-    }
-    else if(event.target.textContent == "Scoreworks") {
-        background.src = "img/project_scoreworks.webm";
-    }
-    else if(event.target.textContent == "Paranoia") {
-        background.src = "img/project_paranoia.webm";
-    }
-    else if(event.target.textContent == "MonoDirector") {
-        background.src = "img/project_monodirector.webm";
-    }
-    else if(event.target.textContent == "Thrusters") {
-        background.src = "img/project_thrusters.webm";
-    }
     
+    currentVideo = projectVideos[event.target.textContent];
+
+    if(activeVideo == currentVideo) {
+        return;
+    }
+
+    background.src = currentVideo;
     background.load();
+    
+    activeVideo = currentVideo;
 }
 
 elements.forEach((element) => {

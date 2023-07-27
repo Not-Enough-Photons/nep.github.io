5ad3e411671da59ae2ec5d8879fd567f00667334
adamdev | Not Enough Photons <adamdevofficial@protonmail.com>
2023-07-26 23:55:39 -0400

    project description page

--- project_dummy.js
+++ project_dummy.js
@@ -6,20 +6,35 @@ class ProjectInfo {
     projectPlatform = "";
 }
 
-fetch("./project_information.json")
-.then(function(response) {
-    return response.json();
-})
-.then(function(data) {
-    console.log(data);
-});
+const json = fetch("nep.github.io/project_information.json")
+.then((request) => { return request.json();})
+.then((data) => console.log(data));
 
-const nameElement = document.querySelector("name");
-const releaseElement = document.querySelector("release");
-const gameElement = document.querySelector("game");
-const platformElement = document.querySelector("platform");
+var baseInfo = new ProjectInfo();
 
-nameElement.textContent = info.projectName;
-releaseElement.textContent = info.projectRelease;
-gameElement.textContent = info.projectGame;
-platformElement.textContent = info.projectPlatform;
+function LoadInfo(name) {
+    baseInfo.projectName = name;
+
+    if(name == "MonoDirector") {
+        baseInfo.projectPlatform = "Valve Index";
+        baseInfo.projectGame = "BONELAB";
+        baseInfo.projectRelease = "2023 TBD";
+    }
+    else if(name == "Paranoia") { 
+        baseInfo.projectPlatform = "Valve Index";
+        baseInfo.projectGame = "BONELAB";
+        baseInfo.projectRelease = "2022";
+    }
+}
+
+const nameElement = document.querySelector("p#name");
+const releaseElement = document.querySelector("p#release");
+const gameElement = document.querySelector("p#game");
+const platformElement = document.querySelector("p#platform");
+
+LoadInfo("Paranoia");
+
+nameElement.textContent = baseInfo.projectName;
+releaseElement.textContent = baseInfo.projectRelease;
+gameElement.textContent = baseInfo.projectGame;
+platformElement.textContent = baseInfo.projectPlatform;

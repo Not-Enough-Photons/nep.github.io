97d306d632b940ed7358517dcb6430f4a56653be
adamdev | Not Enough Photons <adamdevofficial@protonmail.com>
2023-08-07 09:13:49 -0700

    added a gallery to the project page

---
+++ projects/gallery_view.js
@@ -0,0 +1,64 @@
+var galleryView = document.querySelector(".gallery-image-view");
+var galleryViewFooter = document.querySelector(".gallery-view-footer");
+
+var previousImageButton = document.querySelector(".backButton");
+var nextImageButton = document.querySelector(".nextButton");
+var closeButton = document.querySelector(".button-close");
+var pageIndex = document.querySelector(".pageIndex");
+var imageView = document.querySelector(".gallery-image");
+
+let currentIndex = 1;
+let maxIndex = 0;
+
+onIndexChanged();
+
+previousImageButton.addEventListener("click", function() {
+    previousIndex();
+});
+
+nextImageButton.addEventListener("click", function() {
+    nextIndex();
+});
+
+closeButton.addEventListener("click", function() {
+    galleryView.style.opacity = 0;
+    galleryView.style.zIndex = -1;
+});
+
+function onIndexChanged() {
+    pageIndex.textContent = `${currentIndex + 1}/${maxIndex}`
+    imageView.src = images[currentIndex];
+}
+
+function setIndex(index) {
+    currentIndex = index;
+    onIndexChanged();
+}
+
+function setMaxElements(max) {
+    maxIndex = max;
+}
+
+function nextIndex() {
+    if (currentIndex + 1 >= maxIndex) {
+        currentIndex = maxIndex;
+        return;
+    }
+    else {
+        currentIndex++;
+    }
+
+    onIndexChanged();
+}
+
+function previousIndex() {
+    if (currentIndex + 1 <= 1) {
+        currentIndex = 1;
+        return;
+    }
+    else {
+        currentIndex--;
+    }
+
+    onIndexChanged();
+}

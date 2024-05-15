e523f3cfb24d390df47c0848ac35ac1441a2cb61
adamdev <adamdevofficial@protonmail.com>
2024-05-15 05:00:41 -0400

    Update project_listing_script.js

--- scripts/project_listing_script.js
+++ scripts/project_listing_script.js
@@ -1,5 +1,45 @@
 const elements = document.querySelectorAll('#project-parent p#project-button span#project-name');
 
+var request = new Request("https://raw.githubusercontent.com/Not-Enough-Photons/nep.github.io/main/project_information.json");
+
+fetch(request)
+    .then((request) => {
+        if (request.status == 200) {
+            request.json().then((obj) => onDataLoaded(obj));
+        }
+        else {
+            throw new Error("Couldn't load JSON from GitHub link!");
+        }
+    });
+
+function onDataLoaded(json) {
+    const projectElements = document.querySelectorAll('#project-parent > ln > a');
+
+    let mods = [];
+    let games = [];
+    const target = document.querySelector("#project-parent>ln");
+    target.innerHTML = "";
+
+    for (const key in json) {
+        if (key.startsWith("game_")) {
+            let gameRoot = json[key];
+            let gameName = gameRoot["projectName"];
+            let gameRelease = gameRoot["projectRelease"];
+            var content = `<a href="__internalLink__">
+                <p class="project-title" id="project-button">
+                    <span id="project-name">${gameName}</span>
+                    <span class="release-footnote">${gameRelease}</span>
+                    </p>
+                </a>`
+            target.innerHTML += content;
+        }
+        else if (key.startsWith("mod_")) {
+            var text = document.createTextNode(`'<a href="__internalLink__"><p class="project-title" id="project-button"><span id="project-name">__projectName__</span><span class="release-footnote">__releaseDate__ - </span></p></a>"'`);
+        }
+    }
+}
+
+
 const projectVideos = {
     "MagPerception" : "./webm/project_magperception.webm",
     "Hitmarkers": "./webm/project_hitmarkers.webm",
